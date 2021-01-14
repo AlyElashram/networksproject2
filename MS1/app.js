c9143ef26e341a4e3ba048766b93fea06adf471d
AlyElashram <71949028+AlyElashram@users.noreply.github.com>
2021-01-14 04:08:29 +0200

    "Last modifications

--- MS1/app.js
+++ MS1/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 const { nextTick } = require('process');
 var app = express();
 var fs=require('fs');
+var session=require('express-session');
 
 
 
@@ -18,6 +19,11 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret:"secret",
+  resave:false,
+  saveUninitialized:true
+}));
 
 
 
@@ -35,25 +41,88 @@ app.use(express.static(path.join(__dirname, 'public')));
         console.log("Empty Textbox");
         res.render('login',{error:"Please enter information"});
       }
-      else if(check_credentials(x,y)==0){
+      else{
+        var user=check_credentials(x,y)
+      if(user[0]==0){
         console.log("User does not exist");
         res.render('login',{error:"User does not exist"});
       }
-      else if(check_credentials(x,y)==1){
+      else if(user[0]==1){
         console.log("username and passwords match");
+        req.session.user=user[1];
+        req.session.save();
         res.render('home');
       }
-      else if(check_credentials(x,y)==2){
+      else if(user[0]==2){
         console.log("incorrect password");
         res.render('login',{error:"incorrect password"});
       }
-      else if(check_credentials(x,y)==3){
+      else if(user[0]==3){
         console.log("username not registered");
         res.render('login',{error:"username not registered"});
       }
 
 
-      });
+      }});
+
+
+      //Post Method add Dunes to want to read List
+      app.post("/addDunes",function(req,res){
+        
+        for(i in req.session.user.toreadlist ){
+          if(req.session.user.toreadlist[i].name==='dune'){
+            res.render('dune',{error:"Already on your to read list"});
+            return;
+          }
+          
+
+        }
+        
+        var users=getUsers();
+        for(i in users){
+          if(users[i].username===req.session.user.username){
+            console.log({name:'dune',link:'./dune.jpg',href:'/dune'});
+            users[i].toreadlist.push({name:'dune',link:'./dune.jpg',href:'/dune'});
+            req.session.user=users[i];
+            
+          }
+         
+
+        }
+        addUsers(users);
+        res.render('dune',{error:"Added Successfuly"});
+
+
+      })
+      
+      //Post Method add Flies to want to read list
+      app.post("/addFlies",function(req,res){
+
+
+
+      })
+
+      //Post method add grapes to want to read list
+      app.post("/addGrapes",function(req,res){
+
+
+        
+      })
+
+      //Post method add leaves to want to read list
+      app.post("/addLeaves",function(req,res){
+
+
+        
+      })
+
+      //Post method add mockingbird to want to read list
+      app.post("/addMockingBird",function(req,res){
+
+
+        
+      })
+
 
 
 
@@ -74,15 +143,16 @@ app.use(express.static(path.join(__dirname, 'public')));
         }
 
        else if(file.toString()==''){
-          var a=JSON.stringify({user:[x],password:[y]});
+          var a=JSON.stringify([{username:x,password:y,toreadlist:[]}]);
           fs.writeFileSync("users.json",a);
+          req.session.user={username:x,password:y,toreadlist:[]};
           res.render('home');
           
         }
         else{
           file=JSON.parse(file);
-          file.user.push(x);
-          file.password.push(y);
+          file.push({username:x,password:y,toreadlist:[]})
+          req.session.user={username:x,password:y,toreadlist:[]}
           var a=JSON.stringify(file);
           fs.writeFileSync("users.json",a);
           res.render('home');
@@ -134,7 +204,7 @@ app.use(express.static(path.join(__dirname, 'public')));
         })
         //get Request for Fiction 1
         app.get('/dune',function(req,res){
-          res.render('dune');
+          res.render('dune',{error:""});
         });
          //get Request for Fiction 2
          app.get('/mockingbird',function(req,res){
@@ -155,8 +225,8 @@ app.use(express.static(path.join(__dirname, 'public')));
           }
           else{
             file=JSON.parse(file);
-            for(i in file.user){
-              if(file.user[i]==name){
+            for(i in file){
+              if(file[i].username==name){
                 return true;
               }
             }
@@ -175,25 +245,39 @@ app.use(express.static(path.join(__dirname, 'public')));
         // if the username doesn't even exist in the Database it returns 3  
         function check_credentials(name,password){
           var file=fs.readFileSync("users.json");
-          var name_exists=false;
+
           if(file.toString()==''){
-            return 0;
+            return [0];
           }
           file=JSON.parse(file);
-          for(i in file.user){
-            if(file.user[i]==name){
-              name_exists=true;
-              if(file.password[i]==password){
-                return 1;
+          for(i in file){
+            if(file[i].username==name){
+              if(file[i].password==password){
+                return [1,file[i]];
               }
               else{
-                return 2;
+                return [2];
               }
             }
 
           }
-          return 3;
+          return [3];
         }
+        function getUsers(){
+          try{ 
+            var users= JSON.parse(fs.readFileSync('users.json').toString());
+            return users;
+          }catch(e){
+            return [];
+          }
+        }
+        function addUsers(user){
+          var a=JSON.stringify(user);
+          fs.writeFileSync('users.json',a);
+
+        }
+
+        
 // returns 3 if username doesnt exist 2 if password is incorrect and 1 if they match
 
 
